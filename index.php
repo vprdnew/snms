c48021bb50edc881d1926aab73d0db8cd779b4ed
vprd <vishnu@vprd.net>
2016-07-04 18:24:02 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Team Task</title>
+</head>
+
+    <body>
+<button type="submit" style="border: 0; background: transparent">
+    <img src="/images/Btn.PNG" width="90" height="50" alt="submit" />
+</button>
+        </body>
+</html>
